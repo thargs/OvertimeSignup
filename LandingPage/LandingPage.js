0594f1b9b3be137493c0b2db110d111604c741e7
Salvantis <31392152+Salvantis@users.noreply.github.com>
2018-03-30 21:08:26 -0600

    Update LandingPage.js

--- LandingPage/LandingPage.js
+++ LandingPage/LandingPage.js
@@ -104,7 +104,7 @@ $('#logout').on('click', function () {
     event.preventDefault();
     firebase.auth().signOut()
         .then(function () {
-            window.location = '/index.html'
+            window.location = '../index.html'
             console.log('sign out successful')
         })
         .catch(function (error) {
